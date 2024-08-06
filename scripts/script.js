6aff692c013c041f9213b9f494e2119f57d685f7
Jenna Wright <wrightjenna448@gmail.com>
2024-08-06 17:43:22 -0400

    implementing new feedback

--- scripts/script.js
+++ scripts/script.js
@@ -77,25 +77,37 @@ function addNavbarNav(_callback) {
 // Add Navbar Search
 function addNavbarSearch() {
   const currentDiv = document.getElementById("navbarSearchInjection");
-  const searchDiv = document.createElement("div");
-  const searchSVG = document.createElement("svg");
+  if (!currentDiv) return;
+
+  const searchSVG = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
   searchSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
   searchSVG.setAttribute("viewBox", "-893 478 142 142");
   searchSVG.setAttribute("xml:space", "preserve");
-  var pathHold = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  searchSVG.setAttribute("aria-label", "Search Website");
+
+  const pathHold = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
   pathHold.setAttribute(
     "d",
     "M-787.4 568.7h-6.3l-2.4-2.4c7.9-8.7 12.6-20.5 12.6-33.1 0-28.4-22.9-51.3-51.3-51.3s-51.3 22.9-51.3 51.3 22.9 51.3 51.3 51.3c12.6 0 24.4-4.7 33.1-12.6l2.4 2.4v6.3l39.4 39.4 11.8-11.8-39.3-39.5zm-47.3 0c-19.7 0-35.5-15.8-35.5-35.5s15.8-35.5 35.5-35.5 35.5 15.8 35.5 35.5c-.1 19.8-15.8 35.5-35.5 35.5z"
   );
   searchSVG.appendChild(pathHold);
-  searchDiv.innerHTML = searchSVG.outerHTML;
-  currentDiv.parentNode.insertBefore(searchDiv, currentDiv);
-  currentDiv.remove();
+  const searchDiv = document.createElement("div");
+  searchDiv.appendChild(searchSVG);
+
+  currentDiv.parentNode.replaceChild(searchDiv, currentDiv);
 }
 
 // Add Paper Kawaii Logo
 function addPaperKawaiiLogo() {
   const currentDiv = document.getElementById("logoInjection");
+  if (!currentDiv) return;
+
   const logoDiv = document.createElement("div");
   logoDiv.setAttribute("class", "paper-kawaii-logo");
   const logoImg = document.createElement("img");
@@ -103,12 +115,12 @@ function addPaperKawaiiLogo() {
     "src",
     "https://jwright2021.github.io/workingPortfolio/images/origami-cropped-favicon-1.png"
   );
+  logoImg.setAttribute("alt", "Paper Kawaii Logo");
   const logoLabel = document.createElement("a");
   logoLabel.innerText = "Paper Kawaii";
   logoDiv.appendChild(logoImg);
   logoDiv.appendChild(logoLabel);
-  currentDiv.parentNode.insertBefore(logoDiv, currentDiv);
-  currentDiv.remove();
+  currentDiv.parentNode.replaceChild(logoDiv, currentDiv);
 }
 
 // Add Page Constants
@@ -116,95 +128,90 @@ function addPageConstants(_callback) {
   const linksData = baseData.pageConstants;
   const currentDiv = document.getElementById("pageConstantsInjection");
   if (!currentDiv) return;
+  const fragment = document.createDocumentFragment();
 
   const visitPaperShop = document.createElement("div");
-  visitPaperShop.setAttribute("class", "visit-paper-shop");
+  visitPaperShop.className = "visit-paper-shop";
   const visitPaperButton = document.createElement("button");
-  visitPaperButton.setAttribute("class", "origami-buttons");
-  visitPaperButton.innerText = "Visit Paper Kawaii Shop";
+  visitPaperButton.className = "origami-buttons";
+  visitPaperButton.textContent = "Visit Paper Kawaii Shop";
   visitPaperShop.appendChild(visitPaperButton);
-  currentDiv.parentNode.insertBefore(visitPaperShop, currentDiv);
+  fragment.appendChild(visitPaperShop);
 
   const instagramLink = document.createElement("a");
-  instagramLink.setAttribute("href", "https://www.instagram.com/paperkawaii/");
-  instagramLink.setAttribute("target", "_blank");
+  instagramLink.href = "https://www.instagram.com/paperkawaii/";
+  instagramLink.target = "_blank";
   instagramLink.textContent = "@paperkawaii";
   const instagramHead = document.createElement("h2");
-  instagramHead.innerHTML =
-    "Follow " + instagramLink.outerHTML + " on Instagram";
-  currentDiv.parentNode.insertBefore(instagramHead, currentDiv);
+  instagramHead.innerHTML = `Follow ${instagramLink.outerHTML} on Instagram`;
+  fragment.appendChild(instagramHead);
 
   const signUpNewsLetter = document.createElement("div");
-  signUpNewsLetter.setAttribute("class", "sign-up-newsletter");
+  signUpNewsLetter.className = "sign-up-newsletter";
+
   const imageDiv = document.createElement("div");
   const imageSrc = document.createElement("img");
-  imageSrc.setAttribute(
-    "src",
-    "https://jwright2021.github.io/workingPortfolio/images/subscribe-pop2.png"
-  );
+  imageSrc.src =
+    "https://jwright2021.github.io/workingPortfolio/images/subscribe-pop2.png";
   imageDiv.appendChild(imageSrc);
 
   const signUpForm = document.createElement("div");
-  signUpForm.setAttribute("class", "sign-up-form");
+  signUpForm.className = "sign-up-form";
   const freePrints = document.createElement("h3");
-  freePrints.innerText = "Free Printable Origami Paper!";
+  freePrints.textContent = "Free Printable Origami Paper!";
   const printDescription = document.createElement("p");
-  printDescription.innerText =
+  printDescription.textContent =
     "Join Paper Kawaii's newsletter to receive post updates, giveaways, free printables & more.";
-  const newsletterInputOne = document.createElement("div");
-  newsletterInputOne.setAttribute("class", "newsletter-input");
-  const inputName = document.createElement("input");
-  inputName.setAttribute("type", "text");
-  inputName.setAttribute("title", "First Name");
-  inputName.setAttribute("placeholder", "First Name");
-  inputName.setAttribute("value", "");
-  newsletterInputOne.appendChild(inputName);
-  const newsletterInputTwo = document.createElement("div");
-  newsletterInputTwo.setAttribute("class", "newsletter-input");
-  const inputEmail = document.createElement("input");
-  inputEmail.setAttribute("type", "text");
-  inputEmail.setAttribute("title", "Email");
-  inputEmail.setAttribute("placeholder", "Your Email");
-  inputEmail.setAttribute("value", "");
-  newsletterInputTwo.appendChild(inputEmail);
-  const subscribeNewsletter = document.createElement("button");
-  subscribeNewsletter.setAttribute("class", "origami-buttons");
-  subscribeNewsletter.innerText = "Subscribe";
+
+  const createInput = (type, title, placeholder) => {
+    const inputDiv = document.createElement("div");
+    inputDiv.className = "newsletter-input";
+
+    const input = document.createElement("input");
+    input.type = type;
+    input.title = title;
+    input.placeholder = placeholder;
+
+    inputDiv.appendChild(input);
+    return inputDiv;
+  };
 
   signUpForm.appendChild(freePrints);
   signUpForm.appendChild(printDescription);
-  signUpForm.appendChild(newsletterInputOne);
-  signUpForm.appendChild(newsletterInputTwo);
-  signUpForm.appendChild(subscribeNewsletter);
+  signUpForm.appendChild(createInput("text", "First Name", "First Name"));
+  signUpForm.appendChild(createInput("text", "Email", "Your Email"));
 
+  const subscribeNewsletter = document.createElement("button");
+  subscribeNewsletter.className = "origami-buttons";
+  subscribeNewsletter.textContent = "Subscribe";
+
+  signUpForm.appendChild(subscribeNewsletter);
   signUpNewsLetter.appendChild(imageDiv);
   signUpNewsLetter.appendChild(signUpForm);
-  currentDiv.parentNode.insertBefore(signUpNewsLetter, currentDiv);
+  fragment.appendChild(signUpNewsLetter);
 
   const linksPrintables = document.createElement("div");
-  linksPrintables.setAttribute("class", "links-printables");
+  linksPrintables.className = "links-printables";
   const linksHeader = document.createElement("h2");
-  linksHeader.innerText = "Links";
+  linksHeader.textContent = "Links";
   const miscLinks = document.createElement("div");
-  miscLinks.setAttribute("class", "misc-links");
+  miscLinks.className = "misc-links";
 
-  for (i in linksData) {
-    const e = linksData[i];
+  linksData.forEach((e) => {
     const holdLink = document.createElement("a");
-    holdLink.setAttribute("href", e.hrefLink);
-    holdLink.setAttribute("target", "_blank");
+    holdLink.href = e.hrefLink;
+    holdLink.target = "_blank";
     const holdImage = document.createElement("img");
-    holdImage.setAttribute("src", e.imgSrc);
+    holdImage.src = e.imgSrc;
     holdLink.appendChild(holdImage);
     miscLinks.appendChild(holdLink);
-  }
+  });
 
   linksPrintables.appendChild(linksHeader);
   linksPrintables.appendChild(miscLinks);
+  fragment.appendChild(linksPrintables);
 
-  currentDiv.parentNode.insertBefore(linksPrintables, currentDiv);
-
-  currentDiv.remove();
+  currentDiv.parentNode.replaceChild(fragment, currentDiv);
 }
 
 // Add Footer Nav
@@ -212,15 +219,16 @@ function addFooterNav(_callback) {
   const footerLabels = baseData.footerNavigations;
   const currentDiv = document.getElementById("footerInjection");
   if (!currentDiv) return;
-  const footer = document.createElement("div");
-  footer.setAttribute("class", "footer");
-  const footerNav = document.createElement("div");
-  footerNav.setAttribute("class", "footer-nav");
 
-  for (e in footerLabels) {
-    const i = footerLabels[e];
+  const footer = document.createElement("footer");
+  footer.setAttribute("class", "footer");
+  const footerNav = document.createElement("nav");
+  footerNav.setAttribute("class", "footer-nav");
+  footerNav.setAttribute("aria-label", "Footer Navigation");
+
+  for (const e of footerLabels) {
     const newFooterLabel = document.createElement("div");
-    newFooterLabel.innerText = i;
+    newFooterLabel.innerText = e;
     footerNav.appendChild(newFooterLabel);
   }
 
@@ -231,8 +239,7 @@ function addFooterNav(_callback) {
   footer.appendChild(footerNav);
   footer.appendChild(copyrightFooter);
 
-  currentDiv.parentNode.insertBefore(footer, currentDiv);
-  currentDiv.remove();
+  currentDiv.parentNode.replaceChild(footer, currentDiv);
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
